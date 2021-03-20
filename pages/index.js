2364489c5769b1e4285f533ddd7dc9acd6ac0931
Palash Das <dpalash.new@gmail.com>
2021-03-21 00:49:37 +0530

    Extract base page
    Signed-off-by: Palash Das <dpalash.new@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,9 @@
 import React from 'react';
-import clsx from 'clsx';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import {useStyles} from "../src/styles/base";
-import SideBar from "../src/components/navigation/SideBar";
+import {BasePage} from "../src/components/BasePage";
 
-
-const PersistentDrawerLeft = () => {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-
-    return (
-        <div className={classes.root}>
-            <CssBaseline/>
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" noWrap>
-                        Truebid
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <SideBar open={open} handleDrawerClose={handleDrawerClose}/>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <div className={classes.drawerHeader}/>
-                <Typography paragraph>
-                    Content
-                </Typography>
-            </main>
-        </div>
-    );
-};
-
-export default PersistentDrawerLeft
+export default () => (
+    <BasePage
+        header={({classes, drawerOpened}) => 'TrueBid'}
+        content={({classes, drawerOpened}) => (<h1>Welcome</h1>)}
+    />
+)
---
+++ src/components/BasePage.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import clsx from 'clsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {useStyles} from "../styles/base";
+import SideBar from "../components/navigation/SideBar";
+
+const empty = () => (<></>);
+
+export const BasePage = (
+    {
+        header = empty,
+        content = empty,
+    }
+        = {}) => {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => setOpen(true);
+    const handleDrawerClose = () => setOpen(false);
+
+    const childProps = {
+        classes, drawerOpened: open
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline/>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+
+                        {header(childProps)}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <SideBar open={open} handleDrawerClose={handleDrawerClose}/>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+                <div className={classes.drawerHeader}/>
+                <Typography paragraph>
+                    {content(childProps)}
+                </Typography>
+            </main>
+        </div>
+    );
+};
+
