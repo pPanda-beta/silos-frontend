5c163673a91ab629e7a70ba2e50623e074237ca3
ritabrata <ritabrata.m@rapido.bike>
2021-03-30 22:48:10 +0530

    Push frontend changes

--- src/components/listing/CreateListing.js
+++ src/components/listing/CreateListing.js
@@ -15,126 +15,126 @@ import {VerticalTabbedItems} from "../containers/tabbed";
 import {skuImageUrl} from "../../data/common";
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        margin: theme.spacing(2),
-        width: '90%',
-    },
-    submitButton: {
-        margin: theme.spacing(2),
-    }
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    margin: theme.spacing(2),
+    width: '90%',
+  },
+  submitButton: {
+    margin: theme.spacing(2),
+  }
 }));
 
 const Submit = ({classes, listingReq}) => (
-    <QuadStateView
-        sideEffectFn={createListing}
-        initComponentFn={
-            (_, trigger) =>
-                <Button variant="contained"
-                        color="primary"
-                        className={classes.submitButton}
-                        onClick={() => trigger(listingReq)}
-                >
-                    Create Listing
-                </Button>
-        }
-        inProgressComponentFn={
-            () =>
-                <Button variant="contained"
-                        color="primary"
-                        className={classes.submitButton}
-                        disabled
-                >
-                    <pre>Creating  </pre>
-                    <CircularProgress/>
-                </Button>
-        }
-        successComponentFn={
-            ({data}, __, reset) =>
-                <Snackbar open autoHideDuration={2000} onClose={reset}>
-                    <Alert onClose={reset} severity="success">
-                        Created listing! Id: {Object.keys(data)?.[0]}
-                    </Alert>
-                </Snackbar>
-        }
-        errorComponentFn={
-            ({error}, _, reset) =>
-                <Snackbar open autoHideDuration={5000} onClose={reset}>
-                    <Alert onClose={reset} severity="error">
-                        Failed to create listing!
-                        Error : {error?.toString()}
-                    </Alert>
-                </Snackbar>
-        }
-    />
+  <QuadStateView
+    sideEffectFn={createListing}
+    initComponentFn={
+      (_, trigger) =>
+        <Button variant="contained"
+                color="primary"
+                className={classes.submitButton}
+                onClick={() => trigger(listingReq)}
+        >
+          Create Listing
+        </Button>
+    }
+    inProgressComponentFn={
+      () =>
+        <Button variant="contained"
+                color="primary"
+                className={classes.submitButton}
+                disabled
+        >
+          <pre>Creating  </pre>
+          <CircularProgress/>
+        </Button>
+    }
+    successComponentFn={
+      ({data}, __, reset) =>
+        <Snackbar open autoHideDuration={2000} onClose={reset}>
+          <Alert onClose={reset} severity="success">
+            Created listing! Id: {Object.keys(data)?.[0]}
+          </Alert>
+        </Snackbar>
+    }
+    errorComponentFn={
+      ({error}, _, reset) =>
+        <Snackbar open autoHideDuration={5000} onClose={reset}>
+          <Alert onClose={reset} severity="error">
+            Failed to create listing!
+            Error : {error?.toString()}
+          </Alert>
+        </Snackbar>
+    }
+  />
 )
 
 const ListingCreateForm = ({sku, type = null}) => {
-    const classes = useStyles();
-    const [minPrice, setMinPrice] = useState(0);
-    const [maxPrice, setMaxPrice] = useState(0);
-    const [exprDateTime, setExprDateTime] = useState(new Date());
-    const {loggedInUser} = useContext(GlobalContext);
+  const classes = useStyles();
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [exprDateTime, setExprDateTime] = useState(new Date());
+  const {loggedInUser} = useContext(GlobalContext);
 
 
-    return (
-        <div className={classes.container}>
-            <div style={{display: 'block'}}>
-                <Typography variant="h5">
-                    {sku.name}
-                </Typography>
-                <img src={skuImageUrl(sku.sku_id)}
-                     style={{
-                         width: "12rem",
-                         height: "12rem"
-                     }}
-                />
+  return (
+    <div className={classes.container}>
+      <div style={{display: 'block', margin: '0 auto'}}>
+        <Typography variant="h5" style={{'padding-left': '3em'}}>
+          {sku.name}
+        </Typography>
+        <img src={skuImageUrl(sku.sku_id)}
+             style={{
+               width: "30rem",
+               height: "20rem"
+             }}
+        />
 
-            </div>
-            <div>
-                <TextField variant="outlined"
-                           label="Min Price"
-                           type="number"
-                           className={classes.textField}
-                           onChange={(ev) => setMinPrice(ev.target.value)}
-                />
-                <TextField variant="outlined"
-                           label="Max Price"
-                           type="number"
-                           className={classes.textField}
-                           onChange={(ev) => setMaxPrice(ev.target.value)}
+      </div>
+      <div>
+        <TextField variant="outlined"
+                   label="Min Price"
+                   type="number"
+                   className={classes.textField}
+                   onChange={(ev) => setMinPrice(ev.target.value)}
+        />
+        <TextField variant="outlined"
+                   label="Max Price"
+                   type="number"
+                   className={classes.textField}
+                   onChange={(ev) => setMaxPrice(ev.target.value)}
 
-                />
-                <KeyboardDateTimePicker
-                    variant="inline"
-                    ampm={false}
-                    label="Expiration Time"
-                    value={exprDateTime}
-                    onChange={setExprDateTime}
-                    disablePast
-                    format="dd/MM/yy HH:mm"
-                    className={classes.textField}
-                />
-                <Submit classes={classes} listingReq={{
-                    skuId: sku.sku_id, userId: loggedInUser?.user_id,
-                    minPrice, maxPrice, exprDateTime,
-                    sku,
-                }}/>
-            </div>
-        </div>
-    );
+        />
+        <KeyboardDateTimePicker
+          variant="inline"
+          ampm={false}
+          label="Expiration Time"
+          value={exprDateTime}
+          onChange={setExprDateTime}
+          disablePast
+          format="dd/MM/yy HH:mm"
+          className={classes.textField}
+        />
+        <Submit classes={classes} listingReq={{
+          skuId: sku.sku_id, userId: loggedInUser?.user_id,
+          minPrice, maxPrice, exprDateTime,
+          sku,
+        }}/>
+      </div>
+    </div>
+  );
 }
 
 export const CreateListing = ({type}) => {
-    const {skus, error} = useSkus();
-    return (
-        <VerticalTabbedItems
-            items={skus}
-            labelFunc={(t) => t.name}
-            bodyFunc={(t) => <ListingCreateForm sku={t} type={type}/>}
-        />
-    );
+  const {skus, error} = useSkus();
+  return (
+    <VerticalTabbedItems
+      items={skus}
+      labelFunc={(t) => t.name}
+      bodyFunc={(t) => <ListingCreateForm sku={t} type={type}/>}
+    />
+  );
 };
