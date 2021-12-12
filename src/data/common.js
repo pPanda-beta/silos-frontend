1097d1758bb60167ee623c93576827ef4cc957d1
vi <vi@thedarknet.to>
2021-12-12 21:19:30 +0530

    Add utilities for creating forms easily

--- src/data/common.js
+++ src/data/common.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const createEnum = (...keys) => Object.freeze(
     Object.fromEntries(keys.map(t => [t, Symbol(t)]))
@@ -6,12 +6,44 @@ export const createEnum = (...keys) => Object.freeze(
 
 export const skuImageUrl = skuId => `/static/images/${skuId}.jpeg`;
 
-
 export const dateTimeFormatter = (t) => new Date(parseInt(t)).toDateString();
 
-export const asImage = (urlBuilder, props) => (t) => (<img src={urlBuilder(t)} {...props}/>);
+export const asImage = (urlBuilder, props) => (t) => (
+    <img src={urlBuilder(t)} {...props}/>);
 
 export const asSkuImage = (props) => asImage(skuImageUrl, props);
+
+export const combineFns = (...fns) => (arg) => fns.reduce(
+    (resultSoFar, nextFn) => nextFn(resultSoFar), arg);
+
+export const unpackEvent = ev => ev.target.value;
+
+export const eventAware = (fn) => combineFns(unpackEvent, fn);
+
+//TODO: Simplify HOFs
+export const useBoundStateObject = (initialObj = {}) => {
+  const [state, setState] = useState(initialObj);
+  const fieldSetter = (key) => (v) => setState({
+    ...state, [key]: v,
+  });
+
+  const keyBasedStateSetter = (key, ...preFns) => combineFns(
+      ...preFns, fieldSetter(key));
+
+  const keyBinder = ({
+    listenerType = "onChange",
+    injectProp = "value",
+    preProcessors = [unpackEvent],
+    ...others
+  }) => (key, ...additionalProcessors) => ({
+    [injectProp]: state[key],
+    [listenerType]: keyBasedStateSetter(key,
+        ...preProcessors, ...additionalProcessors),
+    ...others,
+  });
+
+  return {state, keyBasedStateSetter, setState, keyBinder};
+}
 
 export const toDataset = (items, columnMappings = null) => ({
         columns: (columnMappings ?? Object.keys(items[0]).map(k => [k, k]))
