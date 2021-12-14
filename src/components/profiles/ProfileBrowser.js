4d0ebd9f4cf2fdb1378208f393f3191e40d000f1
vi <vi@thedarknet.to>
2021-12-15 00:35:05 +0530

    Fix visuals of profile browser

--- src/components/profiles/ProfileBrowser.js
+++ src/components/profiles/ProfileBrowser.js
@@ -1,24 +1,26 @@
 import MUIDataTable from "mui-datatables";
 
 import {profileService} from '../../services/profile';
-import {Card, CardActions, CardContent, Typography} from '@material-ui/core';
+import {Grid, Paper, Typography} from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import {Facebook, LinkedIn} from '@material-ui/icons';
 
 const ProfileCard = ({profile}) => {
 
   return (
-      <Card raised variant='elevation' sx={{spacing: 'gap(1)'}}>
-        <CardContent>
+      <Paper elevation={6}>
+        <Grid container direction="column"
+              alignItems="center" justify="space-evenly">
           <img src={profile?.imagePath} alt={profile?.matrimonyUserName}
-               height={120} // TODO: start using sx
+               height={90} // TODO: start using sx
           />
-        </CardContent>
-        <CardActions>
-          <IconButton><Facebook/></IconButton>
-          <IconButton><LinkedIn/></IconButton>
-        </CardActions>
-      </Card>
+          <Grid container direction="row"
+                alignItems="center" justify="space-evenly">
+            <IconButton size="small"><Facebook/></IconButton>
+            <IconButton size="small"><LinkedIn/></IconButton>
+          </Grid>
+        </Grid>
+      </Paper>
   );
 };
 
@@ -29,6 +31,7 @@ const ProfileBrowser = () => {
     filter: true,
     filterArrayFullMatch: false,
     filterType: 'dropdown',
+    //TODO: use  responsive: 'scrollMaxHeight', to enable fixed height and inner scroll, but that will break the mobile mode
     responsive: 'vertical',
     selectableRows: false,
     print: false,
